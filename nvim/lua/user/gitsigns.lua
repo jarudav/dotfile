d6f5b91e29f21671e1563214a66734f51de76023
Jarudate Vorasee <jarudate.vor@gmail.com>
2022-10-13 14:53:48 +0700

    update gitsign maps

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -1,13 +1,41 @@
-local status_ok, gitsigns = pcall(require, "gitsigns")
-if not status_ok then
+local status, gitsigns = pcall(require, "gitsigns")
+if not status then
 	return
 end
 
 gitsigns.setup({
 	signs = {},
 	on_attach = function(bufnr)
-		vim.keymap.set("n", "[c", require("gitsigns").prev_hunk, { buffer = bufnr })
-		vim.keymap.set("n", "]c", require("gitsigns").next_hunk, { buffer = bufnr })
-		vim.keymap.set("n", "<space>gp", require("gitsigns").preview_hunk, { buffer = bufnr })
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		-- Actions
+		map("n", "<space>hp", gs.preview_hunk)
 	end,
 })
