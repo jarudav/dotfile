a8b105fe7b3606ca323077c69d8f4fea78b7e680
Jarudate Vorasee <jarudate.vor@gmail.com>
2022-10-13 14:54:57 +0700

    update color_icons, close_icon, remove diagnostics_indicator

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -1,26 +1,14 @@
-local status_ok, bufferline = pcall(require, "bufferline")
-if not status_ok then
+local status, bufferline = pcall(require, "bufferline")
+if not status then
 	return
-end
-
-local function diagnostics_indicator(_, _, diagnostics)
-	local symbols = { error = " ", warning = " ", info = " " }
-	local result = {}
-	for name, count in pairs(diagnostics) do
-		if symbols[name] and count > 0 then
-			table.insert(result, symbols[name] .. count)
-		end
-	end
-	result = table.concat(result, " ")
-	return #result > 0 and result or ""
 end
 
 bufferline.setup({
 	options = {
-		sort_by = "insert_after_current",
+		color_icons = true,
 		show_close_icon = false,
+		show_buffer_close_icons = false,
 		diagnostics = "nvim_lsp",
-		diagnostics_indicator = diagnostics_indicator,
 		offsets = {
 			{
 				filetype = "NvimTree",
