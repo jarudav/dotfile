01b35cc74e35a4257b913ddaf3b23edce2a097fc
Jarudate Vorasee <jarudate.vor@gmail.com>
2023-01-16 21:36:40 +0700

    fix new lspsaga

--- nvim/lua/user/lspsaga.lua
+++ nvim/lua/user/lspsaga.lua
@@ -3,25 +3,6 @@ if not status then
 	return
 end
 
-saga.init_lsp_saga({
-	border_style = "rounded",
-	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-	finder_action_keys = {
-		open = "o",
-		vsplit = "v",
-		split = "x",
-		tabe = "t",
-		quit = "q",
-	},
-	code_action_keys = {
-		quit = "q",
-		exec = "<CR>",
-	},
-	definition_action_keys = {
-		edit = "<C-c>o",
-		vsplit = "<C-c>v",
-		split = "<C-c>x",
-		tabe = "<C-c>t",
-		quit = "q",
-	},
+saga.setup({
+	rename = { whole_project = false },
 })
