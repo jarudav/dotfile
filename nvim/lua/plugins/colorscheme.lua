de73f40cc78be0ba08b1c795a2c63d091d52861d
Jarudate Vorasee <jarudate.vor@gmail.com>
2024-05-29 09:37:40 +0700

    update neosolarized.nvim to call vim.cmd

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
-	{
-		"svrana/neosolarized.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = { comment_italics = true },
-		dependencies = { "tjdevries/colorbuddy.nvim" },
-	},
+  {
+    "svrana/neosolarized.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { comment_italics = true },
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+    config = function()
+      vim.cmd("colorscheme neosolarized")
+    end,
+  },
 
-	{ "norcalli/nvim-colorizer.lua", opts = { "*" } },
+  { "norcalli/nvim-colorizer.lua", opts = { "*" } },
 }
